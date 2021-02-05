a2cd14794a5033f216256b34f094bdae01b19557
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2021-02-05 12:30:25 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 let CO2 = 0
+// CO2 Messen
 input.onButtonPressed(Button.A, function () {
     CO2 = SCD30.readCO2()
 })
+// CO2 als Zahlenwert anzeigen
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(CO2)
 })
+// Die Funktion läuft ca. 1 sec.
 function ZeigeCO2 () {
+    // CO2 < 1000ppm
     if (CO2 < 1000) {
         basic.showLeds(`
             . . . . .
@@ -15,6 +19,7 @@ function ZeigeCO2 () {
             . . . . .
             `)
     } else {
+        // CO2 < 2000ppm
         if (CO2 < 2000) {
             basic.showLeds(`
                 . . . . .
@@ -24,6 +29,7 @@ function ZeigeCO2 () {
                 . . . . .
                 `)
         } else {
+            // C02 > 2000ppm
             basic.showLeds(`
                 # # # # #
                 # # # # #
@@ -34,12 +40,15 @@ function ZeigeCO2 () {
             music.playTone(262, music.beat(BeatFraction.Quarter))
         }
     }
+    // Solange leuchten die LED's
     basic.pause(100)
     basic.clearScreen()
+    // Solange bleiben die LED's aus
     basic.pause(900)
 }
 basic.forever(function () {
     CO2 = SCD30.readCO2()
+    // 60 mal 1sec = 1min
     for (let index = 0; index < 60; index++) {
         ZeigeCO2()
     }
